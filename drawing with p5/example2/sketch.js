40bc2fe7902c25ad1a22cbe0c69a6fab4e7193a9
Joseph <92286532+jcurtis182@users.noreply.github.com>
2023-01-24 20:06:10 -0600

    Add files via upload

---
+++ drawing with p5/example2/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+  
+  noStroke();
+  fill(255,0,0,150);  
+  circle(250,175,260);
+
+  fill(0,255,0,150);
+  circle(350,325,260);
+  
+  fill(0,0,255,150);
+  circle(150,325,260);
+}
