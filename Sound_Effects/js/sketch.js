f112bda06a5e63cc4f5e0b5e0e8fb26132bdfdef
Joseph <92286532+jcurtis182@users.noreply.github.com>
2023-03-07 16:02:14 -0600

    Add files via upload

---
+++ Sound_Effects/js/sketch.js
@@ -0,0 +1,76 @@
+var noBonk, bonk, synth, reverb, chorus, autowah, bgm;
+var textInfo = "Click the mouse button to disavow unbecoming behavior.";
+
+function preload(){
+	noBonk = loadImage('assets/noBonk.jpg')
+  bonk = loadImage('assets/bonk.jpg');
+  // bgm = new Tone.Buffer("assets/npc_music.mp3");
+ // bgm.loop = true;
+}
+
+function setup(){
+	createCanvas(800,640);
+  image(noBonk,0,0)
+  
+  synth = new Tone.MembraneSynth({
+    pitchDecay: 0.05,
+    envelope: {
+      attack: 0.001, 
+      decay: 0.1, 
+      sustain: 0, 
+      release: 0.2 
+    }
+  }).toDestination();
+  
+  reverb = new Tone.Reverb({
+    decay: 5,
+    wet: 0.5
+  }).toDestination();
+
+  chorus = new Tone.Chorus({
+    frequency: 1,
+    delayTime: 2.5,
+    wet: 0.5
+  }).toDestination();
+
+  autowah = new Tone.AutoWah({
+    octaves: 1,
+    Q: 9,
+    wet: 0.3
+  }).toDestination();
+
+  synth.connect(reverb).connect(chorus).connect(autowah);
+  
+  bgm = new Tone.Player({
+	  url: "assets/npc_music.mp3",
+	  loop: true,
+	  autostart: true,
+	  onload: function() {
+	    console.log("bgm loaded");
+	  },
+	  onerror: function() {
+	    console.log("bgm error");
+	  }
+	}).toDestination();
+}
+
+function draw(){
+	textSize(24);
+  textAlign(CENTER);
+	text(textInfo,width/2,30);
+}
+
+function mousePressed(){
+	image(bonk,0,0);
+	bonkSound();
+}
+
+function mouseReleased(){
+	image(noBonk,0,0);
+}
+
+function bonkSound(){
+  synth.envelope.attack = 0.001 + Math.random() * 0.01;           //adding in some variety when spamming (a natural response)
+  synth.envelope.release = 0.2 + Math.random() * 0.1;
+  synth.triggerAttackRelease("C1", "8n");
+}
